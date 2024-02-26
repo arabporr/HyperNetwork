14ba63d45af1792c8d6d07f3cec06b22b2076a95
Reza Arabpour <42333732+arabporr@users.noreply.github.com>
2024-02-26 16:58:34 +0000

    Grid added, projection modified to make many versions and save the stuff

---
+++ Grid_Test.py
@@ -0,0 +1,64 @@
+# Setup
+import itertools
+import torch
+import numpy as np
+import projection
+
+seed = 42
+device = (
+    torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
+)
+np.random.seed(seed)
+torch.manual_seed(seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+
+# Sigma, Mu, and Varsigma values
+sigmas = []
+sigmas.append(torch.eye(4))
+# sigma[1], sigma[2], ...
+
+
+mus = []
+long_term_mean = [0.01]
+mean_reversion = [0.5]
+
+for ltm in long_term_mean:
+    for mr in mean_reversion:
+        mu_func_type1 = (
+            lambda X, ind: (ltm - X) * mr + torch.cos(torch.tensor(ind / 10)) * 0.001
+        )
+        mus.append(mu_func_type1)
+
+
+volatility = [0.001]
+varsigmas = []
+for vol in volatility:
+    varsigma_func_type1 = lambda X: torch.ones(X.shape[0]) * vol
+    varsigmas.append(varsigma_func_type1)
+
+
+# TEST CONFIGURATIONS
+params_list_with_states = {
+    "N": [400],
+    "T": [40],
+    "d": [4],
+    "lambda": [0, 0.5, 1],
+    "memory": [0],
+    "sigma": sigmas,
+    "mu": mus,
+    "varsigma": varsigmas,
+}
+
+
+# Generating Data Instance
+params_perm = list(params_list_with_states.values())
+Permutations = list(itertools.product(*params_perm))
+for data_index, params in enumerate(Permutations):
+    parameters = [data_index, params]
+    problem = projection.OU_Projection_Problem(parameters)
+    problem.Auto_Generate()
--- projection.py
+++ projection.py
@@ -1,144 +1,133 @@
-## Standard libraries
-import numpy as np
-
-## Imports for plotting
+# Standard libraries
 import matplotlib.pyplot as plt
-
-## PyTorch
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.utils.data as data
-import torch.optim as optim
-
-## TensorFlow
 import tensorflow as tf
 
-def setup_gpu(seed=42):
-    # Function for setting the seed
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if torch.cuda.is_available(): # GPU operation have separate seed
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-    # Additionally, some operations on a GPU are implemented stochastic for efficiency
-    # We want to ensure that all operations are deterministic on GPU (if used) for reproducibility
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    # Fetching the device that will be used throughout this notebook
-    device = torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
-    print("Using device -> ", device)
-    return device
 
 def fan_diagram(X):
     quantiles = torch.quantile(X, torch.tensor([0.05, 0.5, 0.95]), dim=0)
-    plt.figure(figsize=(16,8))
+    plt.figure(figsize=(16, 8))
     plt.plot(quantiles[0])
     plt.plot(quantiles[1])
     plt.plot(quantiles[2])
     plt.plot(X[0])
 
+
 class Projection_Problem:
-    def __init__(self):
-        self.N = 400
-        self.T = 40
-        self.d = 4
-        self.Sigma = torch.eye(self.d)
-        self.Lambda = 0
-        self.memory = 0
-        self.X = [] 
+
+    def __init__(self, params):
+        self.X = []
         self.Drift = []
         self.Diffusion = []
         self.input_output_pairs = []
-    
-    def mu(self, X, ind):
-        raise NotImplementedError()
-
-    def varsigma(self, X):
-        raise NotImplementedError()
+        (
+            self.N,
+            self.T,
+            self.d,
+            self.lambda_,
+            self.memory,
+            self.sigma,
+            self.mu,
+            self.varsigma,
+        ) = params[1]
 
     def Algorithm3(self):
-        N = self.N
-        T = self.T 
-        d = self.d
-        Mu = self.mu
-        Varsigma = self.varsigma 
-        Sigma = self.Sigma 
-        Lambda = self.Lambda 
-        memory = self.memory
-        X = [] 
+        X = []
         Drift = []
         Diffusion = []
-        for t in range(T+1):
+        for t in range(self.T + 1):
             if t == 0:
-                X.append(torch.zeros(N, d)) # X[-1]
-                Drift.append(torch.zeros(N, d))
-                Diffusion.append(torch.zeros(N, d, d))
-                X.append(torch.zeros(N, d)) # X[0]
-                Drift.append(torch.zeros(N, d)) 
-                Diffusion.append(torch.zeros(N, d, d))
+                X.append(torch.zeros(self.N, self.d))  # X[-1]
+                Drift.append(torch.zeros(self.N, self.d))
+                Diffusion.append(torch.zeros(self.N, self.d, self.d))
+                X.append(torch.zeros(self.N, self.d))  # X[0]
+                Drift.append(torch.zeros(self.N, self.d))
+                Diffusion.append(torch.zeros(self.N, self.d, self.d))
             else:
-                Z = torch.randn(N, d) # N x d 
-                B = torch.bernoulli(torch.tensor([0.5]*N)) # N 
-                drift_t = memory * Mu(X[t-2], t) + (1-memory) * Mu(X[t-1], t) # N x d 
-                S = torch.einsum("n,dk->ndk", (B * Lambda), torch.eye(d)) # N x d x d
-                diffusion_t = S + torch.einsum("n,dk->ndk", Varsigma(X[t-1]), Sigma) # N x d x d
-                x_t = X[t-1] + drift_t + torch.einsum("nk,nkd->nd", Z, diffusion_t) # N x d
+                Z = torch.randn(self.N, self.d)  # N x d
+                B = torch.bernoulli(torch.tensor([0.5] * self.N))  # N
+                drift_t = self.memory * self.mu(X[t - 2], t) + (
+                    1 - self.memory
+                ) * self.mu(
+                    X[t - 1], t
+                )  # N x d
+                S = torch.einsum(
+                    "n,dk->ndk", (B * self.lambda_), torch.eye(self.d)
+                )  # N x d x d
+                diffusion_t = S + torch.einsum(
+                    "n,dk->ndk", self.varsigma(X[t - 1]), self.sigma
+                )  # N x d x d
+                x_t = (
+                    X[t - 1] + drift_t + torch.einsum("nk,nkd->nd", Z, diffusion_t)
+                )  # N x d
                 X.append(x_t)
                 Drift.append(drift_t)
                 Diffusion.append(diffusion_t)
-        X_NTd = torch.stack(X, dim=1) 
+        X_NTd = torch.stack(X, dim=1)
         Drift_NTd = torch.stack(Drift, dim=1)
         Diffusion_NTd = torch.stack(Diffusion, dim=1)
-        self.X = X_NTd[:, 0:, :] # t = -1 to t = T
-        self.Drift = Drift_NTd[:, 0:, :] # t = -1 to t = T
-        self.Diffusion = Diffusion_NTd[:, 0:, :] # t = -1 to t = T
+        self.X = X_NTd[:, 0:, :]  # t = -1 to t = T
+        self.Drift = Drift_NTd[:, 0:, :]  # t = -1 to t = T
+        self.Diffusion = Diffusion_NTd[:, 0:, :]  # t = -1 to t = T
         return self.X
-    
+
     def Algorithm1(self):
-        N = self.N
-        T = self.T 
-        d = self.d
-        Varsigma = self.varsigma 
-        Sigma = self.Sigma 
-        Lambda = self.Lambda 
-        X = self.X 
+        X = self.X
         Drift = self.Drift
-        sigma_2 = torch.matrix_power(Sigma,2)
-        sigma_neg_2 = torch.matrix_power(torch.linalg.pinv(Sigma), 2)
+        sigma_2 = torch.matrix_power(self.sigma, 2)
+        sigma_neg_2 = torch.matrix_power(torch.linalg.pinv(self.sigma), 2)
         Z = []
-        for t in range(2, T+2):
-            x_t_2 = X[:, t-2, :]
-            x_t_1 = X[:, t-1, :]
+        for t in range(2, self.T + 2):
+            x_t_2 = X[:, t - 2, :]
+            x_t_1 = X[:, t - 1, :]
             x_t = X[:, t, :]
             drift_t = Drift[:, t, :]
-            mu_x_t =  x_t_1 + drift_t # N x d
-            inner_power_arg = Lambda * torch.eye(d).reshape((1, d, d)).repeat(N, 1, 1) +  torch.einsum("n,dk->ndk", Varsigma(x_t), Sigma) # N x d x d
+            mu_x_t = x_t_1 + drift_t  # N x d
+            inner_power_arg = self.lambda_ * torch.eye(self.d).reshape(
+                (1, self.d, self.d)
+            ).repeat(self.N, 1, 1) + torch.einsum(
+                "n,dk->ndk", self.varsigma(x_t), self.sigma
+            )  # N x d x d
             inner_power = torch.linalg.matrix_power(inner_power_arg, 2)
-            
-            outer_power_arg_torch = torch.einsum("ndk,lk->ndk", inner_power, sigma_neg_2)
-            outer_power_arg_tf = tf.convert_to_tensor(outer_power_arg_torch)
-            outer_power = torch.from_numpy(tf.linalg.sqrtm(outer_power_arg_tf).numpy()) # N x d x d    
 
-            outer_mul = torch.einsum("ndk,lk->ndk", outer_power, sigma_2) # N x d x d
-            covariance = torch.einsum("n,ndk->ndk", Varsigma(x_t), outer_mul) # N x d x d
+            outer_power_arg_torch = torch.einsum(
+                "ndk,lk->ndk", inner_power, sigma_neg_2
+            )
+            outer_power_arg_tf = tf.convert_to_tensor(outer_power_arg_torch)
+            outer_power = torch.from_numpy(
+                tf.linalg.sqrtm(outer_power_arg_tf).numpy()
+            )  # N x d x d
+
+            outer_mul = torch.einsum("ndk,lk->ndk", outer_power, sigma_2)  # N x d x d
+            covariance = torch.einsum(
+                "n,ndk->ndk", self.varsigma(x_t), outer_mul
+            )  # N x d x d
 
             Y = [mu_x_t, covariance]
-            Z.append([torch.cat((x_t_2, x_t_1),dim=-1), Y])
+            Z.append(
+                [torch.cat((x_t_2, x_t_1, torch.full((self.N, 1), t + 1)), dim=-1), Y]
+            )
         self.input_output_pairs = Z
         return self.input_output_pairs
 
+
 class OU_Projection_Problem(Projection_Problem):
-    def __init__(self):
-        super().__init__()
-        self.long_term_mean = 0.01
-        self.mean_reversion = 0.5
-        self.volitality = 0.001
-    
-    def mu(self, X, ind):
-        return (self.long_term_mean - X) * self.mean_reversion + torch.cos(torch.tensor(ind*10))*0.001
+    def __init__(self, params):
+        self.data_index = params[0]
+        self.params = params
+        super(OU_Projection_Problem, self).__init__(self.params)
 
-    def varsigma(self, X):
-        return torch.ones(X.shape[0]) * self.volitality
-  
-
+    def Auto_Generate(self):
+        self.Algorithm3()
+        self.Algorithm1()
+        PATH = "problem_instance_" + str(self.data_index) + ".pt"
+        result = [
+            self.N,
+            self.T,
+            self.d,
+            self.X,
+            self.Drift,
+            self.Diffusion,
+            self.input_output_pairs,
+        ]
+        torch.save(result, PATH)
